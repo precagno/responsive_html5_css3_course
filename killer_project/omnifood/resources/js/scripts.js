bd9c6cb60beb1803607bab166330e3805488b0a7
pablosrecagno88 <pablosrecagno88@gmail.com>
2018-02-07 16:22:32 -0300

    Sticky nav terminada

--- killer_project/omnifood/resources/js/scripts.js
+++ killer_project/omnifood/resources/js/scripts.js
@@ -1,4 +1,36 @@
 $(document).ready(function(){
-   
     
+    /*For the sticky navigation*/
+    $(".js--section-features").waypoint(function(direction){
+        if(direction=="down"){
+            $("nav").addClass("sticky");
+        }else{
+            $("nav").removeClass("sticky");
+        }
+    },{offset:"60px"});
+    
+    /*Scroll to plans*/
+    $(".js--scroll-to-plans").click(function(){
+        $("html,body").animate({scrollTop:$(".js--section-plans").offset().top},2000);
+    });
+    
+    $(".js--scroll-to-start").click(function(){
+        $("html,body").animate({scrollTop:$(".js--section-features").offset().top},2000);
+    });
+    
+    /* Navigation scroll */
+    $(function() {
+      $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    });
 });
