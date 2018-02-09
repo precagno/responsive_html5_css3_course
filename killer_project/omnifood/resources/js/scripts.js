44cc26f581c0f7ecd50e8ae7bd0e9e9b6acac7ab
pablosrecagno88 <pablosrecagno88@gmail.com>
2018-02-09 12:44:24 -0300

    Parte jquery terminada

--- killer_project/omnifood/resources/js/scripts.js
+++ killer_project/omnifood/resources/js/scripts.js
@@ -49,4 +49,20 @@ $(document).ready(function(){
         $(".js--wp-4").addClass("animated pulse");
     },{offset:"50%"});
     
+    /*Mobile navigation*/
+    $(".js--nav-icon").click(function(){
+        var nav=$(".js--main-nav");
+        var icon=$(".js--nav-icon i");
+        
+        nav.slideToggle(800);
+        
+        if(icon.hasClass("ion-navicon-round")){
+            icon.addClass("ion-close-round");
+            icon.removeClass("ion-navicon-round");
+        }else{
+            icon.addClass("ion-navicon-round");
+            icon.removeClass("ion-close-round");
+        }
+    });
+    
 });
